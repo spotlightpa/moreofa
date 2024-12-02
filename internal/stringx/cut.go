154fab37ae88946309407d956a508b0f7f694fca
Carlana Johnson <mail@carlana.net>
2024-12-02 14:15:08 -0500

    stringx: Gofmt

--- internal/stringx/cut.go
+++ internal/stringx/cut.go
@@ -2,7 +2,6 @@ package stringx
 
 import "strings"
 
-
 func LastCut(s, sep string) (before, after string, found bool) {
 	if i := strings.LastIndex(s, sep); i >= 0 {
 		return s[:i], s[i+len(sep):], true
