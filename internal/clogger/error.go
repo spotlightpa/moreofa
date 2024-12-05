ddbf02bcb3a325e7e30841b25cb9733d6388b589
Carlana Johnson <mail@carlana.net>
2024-12-05 18:12:56 -0500

    clogger: Log errors to Sentry

--- internal/clogger/error.go
+++ internal/clogger/error.go
@@ -1,7 +1,34 @@
 package clogger
 
-import "context"
+import (
+	"context"
+	"net/http"
+
+	"github.com/getsentry/sentry-go"
+)
 
 func LogErr(ctx context.Context, err error) {
-	FromContext(ctx).ErrorContext(ctx, "error", "error", err)
+	l := FromContext(ctx)
+	if hub := sentry.GetHubFromContext(ctx); hub != nil {
+		hub.WithScope(func(scope *sentry.Scope) {
+			hub.CaptureException(err)
+		})
+	} else {
+		l.Warn("sentry not in context")
+	}
+	l.ErrorContext(ctx, "error", "error", err)
+}
+
+func LogRequestErr(r *http.Request, err error) {
+	ctx := r.Context()
+	l := FromContext(ctx)
+	if hub := sentry.GetHubFromContext(ctx); hub != nil {
+		hub.WithScope(func(scope *sentry.Scope) {
+			scope.SetRequest(r)
+			hub.CaptureException(err)
+		})
+	} else {
+		l.Warn("sentry not in context")
+	}
+	l.ErrorContext(ctx, "error", "error", err)
 }
